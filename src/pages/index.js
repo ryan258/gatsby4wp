5c27eb61324f97e623e483e30b7b14977b0c22b3
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-03-05 10:30:41 -0600

    Add data to blog roll.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,43 @@
 import * as React from 'react'
-import { Link as GatsbyLink } from 'gatsby'
+import { Link as GatsbyLink, useStaticQuery, graphql } from 'gatsby'
 import { Box, Container, Heading, Link, Stack, Text } from '@chakra-ui/react'
 
 const HomePage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allWpPost {
+        nodes {
+          id
+          title
+          excerpt
+          uri
+        }
+      }
+    }
+  `)
+
+  // console.log(data) // see our array of node objects
+
+  // destructure our posts from data
+  const { allWpPost } = data
+
   return (
     <Container maxW="x1" centerContent>
       <Heading as={`h1`} m={5}>
         #100daysofgatsby
       </Heading>
       <Stack spacing={8}>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Hello World!</Heading>
-          <Text mt={4}>
-            <span dangerouslySetInnerHTML={{ __html: '<p>This is an excerpt.</p>' }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More
-          </Link>
-        </Box>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Goodbye World!</Heading>
-          <Text mt={4}>
-            <span dangerouslySetInnerHTML={{ __html: '<p>This is an excerpt.</p>' }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More
-          </Link>
-        </Box>
+        {allWpPost.nodes.map(({ id, title, excerpt, uri }) => (
+          <Box key={id} p={5} shadow="md" borderWidth="1px">
+            <Heading as={`h2`}>{title}</Heading>
+            <Text mt={4}>
+              <span dangerouslySetInnerHTML={{ __html: excerpt }} />
+            </Text>
+            <Link as={GatsbyLink} to={uri}>
+              Read More
+            </Link>
+          </Box>
+        ))}
       </Stack>
     </Container>
   )
